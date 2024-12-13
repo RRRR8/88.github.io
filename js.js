b6d979b4a3a7b84d0ea54a525b54c07ee6bb58e6
RRRR8 <166377229+RRRR8@users.noreply.github.com>
2024-12-13 01:39:48 +0000

    Delete js.js

--- js.js
+++
@@ -1,7 +0,0 @@
-function perelet(){
-	document.getElementById("three").value=document.getElementById("one").innerText;
-	document.getElementById("four").value==document.getElementById("two").innerText;
-}
-function vnimanie(){
-	alert("Спасибо за заказ");
-}
